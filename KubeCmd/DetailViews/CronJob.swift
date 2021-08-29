d57a5c406a8012222da576963f423503c67415b1
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2021-08-29 22:16:46 +0100

    adds SwiftCron

--- KubeCmd/DetailViews/CronJob.swift
+++ KubeCmd/DetailViews/CronJob.swift
@@ -7,86 +7,92 @@
 
 import SwiftUI
 import SwiftkubeModel
+import SwiftCron
 
 
 struct SuspendButton: View {
-    @EnvironmentObject var resources: ClusterResources
-    let cronJob:batch.v1beta1.CronJob
-    let suspended:Bool
-    init(cronJob:batch.v1beta1.CronJob) {
-        self.suspended = cronJob.spec?.suspend ?? false
-        self.cronJob = cronJob
-    }
-    var body: some View {
-        if suspended {
-            Button(action: unsuspendCronJob, label: {
-                Text("Unsuspend")
-            })
-        } else {
-            Button(action: suspendCronJob, label: {
-                Text("Suspend")
-            })
-        }
-    }
-    func unsuspendCronJob() -> Void {
-        resources.unsuspendCronJob(cronjob: self.cronJob)
-    }
-    func suspendCronJob() -> Void {
-        resources.suspendCronJob(cronjob: self.cronJob)
-    }
+	@EnvironmentObject var resources: ClusterResources
+	let cronJob:batch.v1beta1.CronJob
+	let suspended:Bool
+	init(cronJob:batch.v1beta1.CronJob) {
+		self.suspended = cronJob.spec?.suspend ?? false
+		self.cronJob = cronJob
+	}
+	var body: some View {
+		if suspended {
+			Button(action: unsuspendCronJob, label: {
+				Text("Unsuspend")
+			})
+		} else {
+			Button(action: suspendCronJob, label: {
+				Text("Suspend")
+			})
+		}
+	}
+	func unsuspendCronJob() -> Void {
+		resources.unsuspendCronJob(cronjob: self.cronJob)
+	}
+	func suspendCronJob() -> Void {
+		resources.suspendCronJob(cronjob: self.cronJob)
+	}
 }
 
 struct CronJob: View {
-    let cronJob:batch.v1beta1.CronJob
-    init(res:KubernetesAPIResource) {
-        self.cronJob = res as! batch.v1beta1.CronJob
-    }
-    var body: some View {
-        VStack(alignment: .leading, spacing: CGFloat(5), content: {
-            if let metadata = cronJob.metadata {
-                MetaDataSection(metadata: metadata)
-                Divider().padding(.vertical, 30)
-            }
-            Text("Status").font(.title2)
-            Text("Schedule: \(cronJob.spec?.schedule ?? "error")")
-            Text("Suspended: \(String(cronJob.spec?.suspend ?? true))")
-            Divider().padding(.vertical, 30)
-            Text("Spec").font(.title2)
-        })
-    }
+	let cronJob:batch.v1beta1.CronJob
+	init(res:KubernetesAPIResource) {
+		self.cronJob = res as! batch.v1beta1.CronJob
+	}
+	var body: some View {
+		VStack(alignment: .leading, spacing: CGFloat(5), content: {
+			if let metadata = cronJob.metadata {
+				MetaDataSection(metadata: metadata)
+				Divider().padding(.vertical, 30)
+			}
+			if let spec = cronJob.spec {
+				Text("Spec").font(.title2)
+				if let parsedCronExpression = CronExpression(cronString: "\(spec.schedule) *") {
+					Text("Schedule: \(parsedCronExpression.longDescription)")
+					if let nextSchedualedDate = parsedCronExpression.getNextRunDateFromNow() {
+						Text("Next Scheduled: \(nextSchedualedDate.description)")
+					}
+				}
+				Text("Suspended: \(String(spec.suspend ?? true))")
+			}
+		})
+	}
 }
 
 struct TriggerCronJobButton : View {
-    let cronJob:batch.v1beta1.CronJob
-    @EnvironmentObject var resources: ClusterResources
-    var body: some View {
-        Button(action: triggerCronJob, label: {
-            Text("Trigger")
-        })
-    }
-    func triggerCronJob() -> Void {
-        let job = createJobFromCronJob(cronJob: self.cronJob)
-        resources.addJob(job: job)
-    }
+	let cronJob:batch.v1beta1.CronJob
+	@EnvironmentObject var resources: ClusterResources
+	var body: some View {
+		Button(action: triggerCronJob, label: {
+			Text("Trigger")
+		})
+	}
+	func triggerCronJob() -> Void {
+		let job = createJobFromCronJob(cronJob: self.cronJob)
+		resources.addJob(job: job)
+	}
 }
 
 func createJobFromCronJob(cronJob:batch.v1beta1.CronJob) -> batch.v1.Job {
-    let tmp = cronJob.spec?.jobTemplate.spec
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "yyyyMMdd-HHmmss-SSS"
-    let dateStr = dateFormatter.string(from: Date())
-    let x = "\(cronJob.name ?? "terrible-error")-manual-\(dateStr)"
-    var existingMetadata = cronJob.metadata
-    existingMetadata?.name = x
-    var job = batch.v1.Job()
-    existingMetadata?.resourceVersion = nil
-    job.spec = tmp
-    job.metadata = existingMetadata
-    return job
+	let tmp = cronJob.spec?.jobTemplate.spec
+	let dateFormatter = DateFormatter()
+	dateFormatter.dateFormat = "yyyyMMdd-HHmmss-SSS"
+	let dateStr = dateFormatter.string(from: Date())
+	let x = "\(cronJob.name ?? "terrible-error")-manual-\(dateStr)"
+	var existingMetadata = cronJob.metadata
+	existingMetadata?.name = x
+	var job = batch.v1.Job()
+	existingMetadata?.resourceVersion = nil
+	job.spec = tmp
+	job.metadata = existingMetadata
+	return job
 }
 
 struct CronJob_Previews: PreviewProvider {
-    static var previews: some View {
-        CronJob(res: ClusterResources.dummyCronJob() as KubernetesAPIResource)
-    }
+	static var previews: some View {
+		CronJob(res: ClusterResources.dummyCronJob() as KubernetesAPIResource)
+	}
 }
