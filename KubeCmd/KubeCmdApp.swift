ba7f6efd900e6db61f69f2ccb812bff3011cf5dc
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2021-08-29 18:56:41 +0100

    cleaned up splash screen

--- KubeCmd/KubeCmdApp.swift
+++ KubeCmd/KubeCmdApp.swift
@@ -7,40 +7,56 @@
 
 import SwiftUI
 import SwiftkubeClient
+import UniformTypeIdentifiers
+
+struct BlueButton: ButtonStyle {
+	func makeBody(configuration: Configuration) -> some View {
+		configuration.label
+			.padding()
+			.background(Color.blue)
+			.foregroundColor(.white)
+			.clipShape(Capsule())
+	}
+}
 
 @main
 struct KubeCmdApp: App {
-    let persistenceController = PersistenceController.shared
-    
-    @State var client:KubernetesClient? = nil
-    
-    func loadKubernetesClientFromConfig(client: KubernetesClient) -> Void {
+	let persistenceController = PersistenceController.shared
+	
+	@State var client:KubernetesClient? = nil
+	
+	func loadKubernetesClientFromConfig(client: KubernetesClient) -> Void {
 		self.client = client
-    }
-
-    var body: some Scene {
-        WindowGroup {
-            if let client = client {
-                ContentView(resources: ClusterResources(client: client))
-                    .environment(\.managedObjectContext, persistenceController.container.viewContext)
-            } else {
-                VStack{
-                    VStack{
-                        Text("Load from config file").font(.title2)
-                        OpenConfigFileButton(cb: loadKubernetesClientFromConfig)
-                    }.padding(.all, 20).frame(alignment: .leading)
-                    Divider()
-                    VStack{
-                        Text("Load from kubectl").font(.title2)
-                        Button("load local file"){
-                            client = KubernetesClient()
-                        }
-                    }.padding(.all, 20)
-                }.frame(width: 400, alignment: .leading)
-            }
-        }
-        .commands {
-            SidebarCommands()
-        }
-    }
+	}
+	
+	var body: some Scene {
+		WindowGroup {
+			if let client = client {
+				ContentView(resources: ClusterResources(client: client))
+					.environment(\.managedObjectContext, persistenceController.container.viewContext)
+			} else {
+				VStack{
+					Image("splash-icon").resizable().scaledToFit().frame(width: 180, height: 180, alignment: .center).padding(.all, 15)
+					Text("KubeCmd").font(.largeTitle)
+					Button("Open", action: openFile).buttonStyle(LinkButtonStyle()).padding(.vertical, 40).font(.title2)
+				}.frame(width: 400, alignment: .center)
+			}
+		}
+		.commands {
+			SidebarCommands()
+		}
+	}
+	func openFile() -> Void {
+		let panel = NSOpenPanel()
+		panel.allowsMultipleSelection = false
+		panel.canChooseDirectories = false
+		panel.allowedContentTypes = [UTType(filenameExtension: "yaml")!]
+		if panel.runModal() == .OK {
+			print(panel.url?.path ?? "<none>")
+			if let u = panel.url {
+				let url = URL(fileURLWithPath: u.path)
+				self.client = KubernetesClient(fromURL: url)
+			}
+		}
+	}
 }
