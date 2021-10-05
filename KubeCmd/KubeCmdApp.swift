2c8cfdf17b98440d5e1cdacaaa6c23119d9a8f89
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2021-10-05 12:18:42 +0100

    removed undo/redo

--- KubeCmd/KubeCmdApp.swift
+++ KubeCmd/KubeCmdApp.swift
@@ -53,6 +53,7 @@ struct KubeCmdApp: App {
 			SidebarCommands()
 			DataCommands(refreashable: self.client != nil, reload: reloadURL)
 			CommandGroup(replacing: .newItem) {}
+			CommandGroup(replacing: .undoRedo) {}
 		}
 	}
 	func openFile() -> Void {
