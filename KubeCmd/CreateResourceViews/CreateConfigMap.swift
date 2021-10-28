7c6b7191b0b3a0c44bdf1bf63d07d75bf11a0013
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2021-10-28 18:47:11 +0100

    added key value interface to create forms

--- KubeCmd/CreateResourceViews/CreateConfigMap.swift
+++ KubeCmd/CreateResourceViews/CreateConfigMap.swift
@@ -10,10 +10,28 @@ import SwiftkubeModel
 
 struct CreateConfigMap: View {
 	@State private var name: String = ""
+	@State private var keyValuePairs: [keyValuePair] = []
+	@State private var keyStr: String = ""
+	@State private var valueStr: String = ""
 	var body: some View {
 		Form{
 			Text("New ConfigMap").font(.title2)
 			TextField("Name", text: $name)
+			Section("KeyValue Pairs") {
+				ForEach(keyValuePairs, content: { x in
+					VStack{
+						Text(x.id)
+						Text(x.value)
+					}.padding(10)
+				})
+				TextField("Key", text: $keyStr)
+				TextField("Value", text: $valueStr)
+				Button("Add"){
+					keyValuePairs.append(keyValuePair(id: keyStr, value: valueStr))
+					keyStr = ""
+					valueStr = ""
+				}.disabled(keyStr.isEmpty || valueStr.isEmpty)
+			}
 		}.padding(40).frame(width: 500, height: 300, alignment: .center)
 	}
 }
--- KubeCmd/CreateResourceViews/CreateSecret.swift
+++ KubeCmd/CreateResourceViews/CreateSecret.swift
@@ -7,18 +7,41 @@
 
 import SwiftUI
 
+struct keyValuePair : Identifiable {
+	var id: String
+	var value: String
+}
+
 struct CreateSecret: View {
 	@State private var name: String = ""
-    var body: some View {
+	@State private var keyValuePairs: [keyValuePair] = []
+	@State private var keyStr: String = ""
+	@State private var valueStr: String = ""
+	var body: some View {
 		Form{
 			Text("New Secret").font(.title2)
 			TextField("Name", text: $name)
+			Section("KeyValue Pairs") {
+				ForEach(keyValuePairs, content: { x in
+					VStack{
+						Text(x.id)
+						Text(x.value)
+					}.padding(10)
+				})
+				TextField("Key", text: $keyStr)
+				TextField("Value", text: $valueStr)
+				Button("Add"){
+					keyValuePairs.append(keyValuePair(id: keyStr, value: valueStr))
+					keyStr = ""
+					valueStr = ""
+				}.disabled(keyStr.isEmpty || valueStr.isEmpty)
+			}
 		}.padding(40).frame(width: 500, height: 300, alignment: .center)
-    }
+	}
 }
-
-struct CreateSecret_Previews: PreviewProvider {
-    static var previews: some View {
-        CreateSecret()
-    }
-}
+//
+//struct CreateSecret_Previews: PreviewProvider {
+//	static var previews: some View {
+//		CreateSecret()
+//	}
+//}
